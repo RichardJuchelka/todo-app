49f42b305b4784093fe72dc4939d36a56fada55d
Richard Juchelka <richardj@kentico.com>
2020-10-26 22:11:47 +0100

    Introduce List component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 import { useDataLoader } from '../hooks/useDataLoader';
 import { todolist } from '../queries/todoList';
-import { TodoListItem } from './Todo';
+import { List } from '../component-library/List';
+import { TodoListItem } from './TodoListItem';
 
 export const App: React.FC = () => {
   const [data] = useDataLoader(todolist);
 
   return (
-    <div>
-      {data?.map((todo) => (
-        <TodoListItem
-          key={todo.id}
-          todo={todo}
-        />
-      ))}
-    </div>
+    <List
+      data={data || []}
+      renderItem={(todoItem) => <TodoListItem key={todoItem.id} todo={todoItem} />}
+    />
   );
 };
